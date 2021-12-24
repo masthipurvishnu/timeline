b161980f225391a9f4f321c80c44896831fbbdc5
Vishnu <masthipurvishnu@gmail.com>
2021-12-24 12:10:57 -0600

    Timeline component creation

---
+++ src/components/Timeline.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-card style="border:1px solid red">
+    Timeline timeline timeline
+  </v-card>
+</template>
+
+<script>
+
+export default {
+  name: 'Timeline',
+
+  data: () => ({
+    //
+  }),
+}
+</script>
